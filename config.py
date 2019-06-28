a21b6c0fc5f4a9f300fd1f6d68e894069b12e803
sebpy <sebastien.fantinati@gmail.com>
2019-06-28 14:56:17 +0200

    Add api key mpas, url and stop words

--- config.py
+++ config.py
@@ -1,1 +1,5 @@
 SECRET_KEY = "#d#JCqTTW\nilK\\7m\x0bp#\tj~#H"
+API_KEY_MAPS = "AIzaSyCp1-r_Zbe4vqcOliKetxr-hG71j3PKc5U"
+
+URL_API_WIKI = "https://fr.wikipedia.org/w/api.php"
+URL_API_GMAPS = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json"
